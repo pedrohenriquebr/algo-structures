97506bcb909fd2cb221ad2d5571cf484a014b756
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-03-28 03:01:56 -0300

    Add Bubble Sort

---
+++ algorithms/bubblesort.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+data  = [9, 5, 1, 3, 4, 7, 8,2]
+
+def bubblesort(v: List[int]) -> List[int]:
+    for i in range(0,len(v)):
+        for j in range(0,len(v)-1):
+            if v[j] > v[j+1]:
+                v[j], v[j+1] = v[j+1], v[j]
+    return v
+
+print(bubblesort(data))
+
